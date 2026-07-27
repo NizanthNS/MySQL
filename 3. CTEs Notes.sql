622cae6faaa0a0aec1b0b6b9437273c6f98ad16b
Nizanth NS <nizanthns10@gmail.com>
2026-07-27 19:53:31 +0530

    Update 3. CTEs Notes.sql

--- 3. CTEs Notes.sql
+++ 3. CTEs Notes.sql
@@ -1,1 +1,12 @@
 
+-- A CTE (Common Table Expression) is a temporary named result set that exists only for the execution of a single SQL statement.
+
+-- It helps you break a complex query into smaller, easier-to-read steps.
+
+-- Example
+
+WITH CTE AS (
+    SELECT ...
+)
+SELECT *
+FROM CTE;
